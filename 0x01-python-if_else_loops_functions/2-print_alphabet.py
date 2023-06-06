29c757aaf3f1c813bc2f7cd3ba55ca5b42f0d906
kingStaff <karimawudulai@gmail.com>
2023-06-06 12:08:34 -0700

    2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -3,4 +3,3 @@
 """Write a code snippet to display the lowercase alphabet without including a new line character after each letter."""
 for letter in range(97, 123):
     print("{}".format(chr(letter)), end="")
-which of the above is coorect
