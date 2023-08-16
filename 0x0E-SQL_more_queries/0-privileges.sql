c8b855e1f72daa1e404eb97fafe23eb61daed5ae
kingStaff <karimawudulai@gmail.com>
2023-08-16 06:35:10 -0700

    completed task 0

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,13 +1,7 @@
 -- lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server.
 
--- Create user user_0d_1 if it doesn't exist
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+-- List privileges for user_0d_1
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
 
--- Grant all privileges to user_0d_1
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-
--- Create user user_0d_2 if it doesn't exist
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
-
--- Grant all privileges to user_0d_2
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
+-- List privileges for user_0d_2
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
